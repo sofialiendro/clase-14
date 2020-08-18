86827d979e8f991306b5d16a06ee8b6b452d6695
Sofia Liendro <sofialiendro.s@gmail.com>
2020-08-17 22:02:07 -0300

    Borro comentarios

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// let gusto1 = prompt("Decime tu gusto de helado favorito.")
-// let gusto2 = prompt("Decime tu segundo gusto de helado favorito.")
-// let gusto3 = prompt("Decime tu tercer gusto de helado favorito.")
-
-// alert (`Aquí tenés tu helado de ${gusto1}, ${gusto2} y ${gusto3}.`)
-
-
-
-//
-
-
-
 let funcionDeEjemplo = () => {
     let gusto1 = prompt("Decime tu gusto de helado favorito.")
     let gusto2 = prompt("Decime tu segundo gusto de helado favorito.")
